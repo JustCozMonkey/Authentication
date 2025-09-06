2e486275752b5bd6a7b3b3c2277fe2c7ff85df0d
Drapiciuc Daniel <126782377+JustCozMonkey@users.noreply.github.com>
2025-09-06 22:25:08 +0300

    forms added

--- auth-site/client/src/pages/Register.tsx
+++ auth-site/client/src/pages/Register.tsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const Register = () => {
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPass] = useState<string>("")
+
+    const submitHandler = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        console.log({ name, email, password })
+    }
     return (
-        <div>
-            <h1>Registration</h1>
+        <div className='bg-blue-300 w-11/12 sm:w-160 px-8 py-16 rounded-tl-3xl rounded-br-3xl hover:shadow-2xl'>
+            <p className='mb-6 text-3xl text-center font-mono font-bold'>
+                Create New Account
+            </p>
+            <form onSubmit={submitHandler} className=' flex flex-col h-1/2 gap-6'>
+                <label className="flex flex-col text-gray-700 font-medium gap-2">
+                    Name:
+                    <input
+                        className='bg-gray-100 rounded-sm border-1 border-black'
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
+                <label className="flex flex-col text-gray-700 font-medium gap-2">
+                    Email:
+                    <input
+                        className='bg-gray-100 rounded-sm border-1 border-black'
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label className="flex flex-col text-gray-700 font-medium gap-2">
+                    Password:
+                    <input
+                        className='bg-gray-100 rounded-sm border-1 border-black'
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPass(e.target.value)}
+                    />
+                </label>
+                <button className='bg-blue-400 w-1/4 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition' type='submit'>Submit</button>
+            </form>
         </div>
+
+
     )
 }
 
--- auth-site/client/src/pages/SIgnIn.tsx
+++ auth-site/client/src/pages/SIgnIn.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SIgnIn = () => {
+    const [email, setEmail] = useState<string>("")
+    const [password, setPass] = useState<string>("")
+
+    const submitHandler = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        console.log({ email, password })
+    }
     return (
-        <div>
-            <h1>Sign In</h1>
+
+        <div className='bg-blue-300 w-11/12 sm:w-160 px-8 py-16 rounded-tl-3xl rounded-br-3xl hover:shadow-2xl'>
+            <p className='mb-6 text-3xl text-center font-mono font-bold'>
+                Create New Account
+            </p>
+            <form onSubmit={submitHandler} className=' flex flex-col h-1/2 gap-6'>
+                <label className="flex flex-col text-gray-700 font-medium gap-2">
+                    Email:
+                    <input
+                        className='bg-gray-100 rounded-sm border-1 border-black'
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label className="flex flex-col text-gray-700 font-medium gap-2">
+                    Password:
+                    <input
+                        className='bg-gray-100 rounded-sm border-1 border-black'
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPass(e.target.value)}
+                    />
+                </label>
+                <button className='bg-blue-400 w-1/4 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition' type='submit'>Submit</button>
+            </form>
+            <p className='text-2xl mt-6 font-bold'>New Customers</p>
+            <p className='text-lg mt-3'>If you don’t have an account yet, you can <Link className='text-red-100 hover:text-red-600' to={"/registration"}>create an account</Link> here.</p>
         </div>
     )
 }
